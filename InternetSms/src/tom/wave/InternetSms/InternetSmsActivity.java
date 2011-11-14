f210832c473fba464b49a5d9584eaeca3ba01c66
hagent <alextomin@list.ru>
2011-11-14 23:20:56 +0300

    AsyncTask test

--- InternetSms/src/tom/wave/InternetSms/InternetSmsActivity.java
+++ InternetSms/src/tom/wave/InternetSms/InternetSmsActivity.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,14 +14,15 @@ import android.widget.TextView;
 public class InternetSmsActivity extends Activity {
 	TextView textView;
 	TextView button;
-	
-	/** Called when the activity is first created. */
-    @Override
+    final int loadDialogId = 14;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        textView = (TextView) findViewById(R.id.label);
+        textView = (TextView) findViewById(R.id.editText);
         button = (TextView) findViewById(R.id.button1);
         
         button.setOnClickListener(new View.OnClickListener() {
@@ -31,13 +34,41 @@ public class InternetSmsActivity extends Activity {
 					e.printStackTrace();
 				}
             }
-
-			private void onSaveButtonClicked() throws IOException {
-				textView.setText("gggg");
-				Object doc = Jsoup.connect("http://en.wikipedia.org/").get();
-//				Elements newsHeadlines = doc.select("#mp-itn b a");
-				textView.setText(doc.getClass().getName());
-			}
         });
     }
+    
+	protected ProgressDialog onCreateDialog(int dialogId){
+		if(dialogId == loadDialogId){
+			ProgressDialog progress = new ProgressDialog(this);
+			progress.setMessage("Loading...");
+	        return progress;
+		}
+		
+		return null;
+	}
+    
+	private void onSaveButtonClicked() throws IOException {
+		new PageLoader().execute("bash.org.ru");
+	}
+	
+	public class PageLoader extends AsyncTask<String, Integer, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+//			Object doc = Jsoup.connect("http://en.wikipedia.org/").get();
+//			Elements newsHeadlines = doc.select("#mp-itn b a");
+			 publishProgress(1);
+	        return "hee";
+	    }
+		
+		@Override
+	    protected void onProgressUpdate(Integer... progress) {
+			showDialog(loadDialogId);
+	    }
+	    
+	    @Override
+	    protected void onPostExecute(String result) {
+	    	dismissDialog(loadDialogId);
+	    	textView.setText(result);
+	    }
+	}
 }
